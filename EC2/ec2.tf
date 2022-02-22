7618ec259313b284cb77e1a0a1296ac11abbc56b
venkatn2607 <venkatn2607@gmail.com>
2022-02-22 11:53:20 +0530

    EC2 server creation

---
+++ EC2/ec2.tf
@@ -0,0 +1,16 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#EC2 instance creation
+resource "aws_instance" "ec2-server" {
+  ami           = "ami-033b95fb8079dc481"
+  instance_type = "t2.micro"
+  key_name      = "aws-key"
+
+  tags = {
+    Name = "Terraform-Server"
+  }
+
+}
