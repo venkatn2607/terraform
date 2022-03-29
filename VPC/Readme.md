cf03b476194656941c461f2d148e7da7d7a90030
venkatn2607 <venkatn2607@gmail.com>
2022-03-29 10:50:17 +0530

    valaxy code added

---
+++ VPC/Readme.md
@@ -0,0 +1,9 @@
+1. VPC
+2. IGW --> associate to VPC
+3. NAT
+4. private subnet
+5. public subnet
+6. private route table --> associate to private subnet
+7. public route table --> associate to public subnet
+8. private route --> add route to route the traffic to NAT
+9. public route --> add route to route the traffic to IGW
--- VPC/vpc.tf
+++ VPC/vpc.tf
@@ -1,1 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "venkat-vpc"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "venkat-igw"
+  }
+}
